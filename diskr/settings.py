00af2e4e0a7a6b156b0f5eaf3e718c62a7226c29
alain-nambi <alainnambi@gmail.com>
2022-12-06 14:32:54 +0300

    importing os and load_dotenv to get env file

--- diskr/settings.py
+++ diskr/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'diskr',
         'USER': 'diskr',
-        'PASSWORD': 'diskr',
+        'PASSWORD': os.environ.get('POSTGRESQL_PASSWORD'),
         'HOST': '',
         'PORT': '5432',
     }
