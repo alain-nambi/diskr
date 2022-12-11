4ec78c4b2d4687507e05ef857a4b6727c5997eb2
alain-nambi <alainnambi@gmail.com>
2022-12-11 21:00:32 +0300

    create dict models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+ARTISTS = {
+    'Eminem': {'name': 'Eminem'},
+    'Selena Gomez': {'name': 'Selena Gomez'},
+    'Ed Sheeran': {'name': 'Ed Sheeran'}
+}
+
+ALBUMS = [
+    {
+        'name': 'Lose Yourself', 
+        'artists': [ARTISTS['Eminem']]
+    },
+    {
+        'name': 'Let Somebody Go', 
+        'artists': [ARTISTS['Selena Gomez']]
+    },
+    {
+        'name': 'Perfect', 
+        'artists': [ARTISTS['Ed Sheeran']]
+    },
+]
