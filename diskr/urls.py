4e5141abb8cc615635c111382b0763cf6dacfc35
alain-nambi <alainnambi@gmail.com>
2022-12-11 19:37:13 +0300

    show directly views store on main url '/'

--- diskr/urls.py
+++ diskr/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from store import views
+
+
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
 ]
