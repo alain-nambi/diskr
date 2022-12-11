70d3e78f0dd3744e8924172716fad2e6305e54a4
alain-nambi <alainnambi@gmail.com>
2022-12-11 21:01:24 +0300

    define listing function and return HttpResponse

--- store/views.py
+++ store/views.py
@@ -1,7 +1,17 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import ALBUMS
+
 # Create your views here.
 def index(request):
     message = f"Bonjour le monde !!!"
     return HttpResponse(message)
+
+def listing(request):
+    albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
+    message = """<ul>{}</ul>""".format("\n".join(albums))
+
+    return HttpResponse(message)
+
+
